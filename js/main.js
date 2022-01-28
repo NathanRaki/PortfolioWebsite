cf97ac446058777e89c84940f79a2f29be8fc2e1
NathanRaki <nathan.raklure@gmail.com>
2022-01-28 05:38:47 +0100

    Added skills

--- js/main.js
+++ js/main.js
@@ -22,9 +22,32 @@ if (navClose)
 /*============== REMOVE MOBILE MENU ==============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction()
+{
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*============== SKILLS ACCORDION ==============*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills()
+{
+    let itemClass = this.parentNode.className
+
+    for (i = 0; i < skillsContent.length; i++)
+    {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if (itemClass === 'skills__content skills__close')
+    {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
